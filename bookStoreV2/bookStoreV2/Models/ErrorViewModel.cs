496af7eb6970bbb71f9da3d127838e36fbfc5ba0
surya-ast <surya.astana71@gmail.com>
2021-12-28 15:02:00 +0700

    project bookstore MVC .NET5

---
+++ bookStoreV2/bookStoreV2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace bookStoreV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
