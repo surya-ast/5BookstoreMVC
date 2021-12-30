00b2c2a7820fb2c6c9e84030538115005d91b52f
surya-ast <surya.astana71@gmail.com>
2021-12-30 10:27:28 +0700

    creating Customer AREAS

--- bookStoreV2/bookStoreV2/Areas/Customer/Controllers/HomeController.cs
+++ bookStoreV2/bookStoreV2/Areas/Customer/Controllers/HomeController.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using bookStoreV2.Models.ViewModels;
 
-namespace bookStoreV2.Controllers
+namespace bookStoreV2.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- bookStoreV2/bookStoreV2/Startup.cs
+++ bookStoreV2/bookStoreV2/Startup.cs
@@ -63,7 +63,7 @@ namespace bookStoreV2
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
