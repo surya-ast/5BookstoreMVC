cb0e88655513ab906bfb95f6fc937d1de562e167
surya-ast <surya.astana71@gmail.com>
2022-01-03 08:09:45 +0700

    menambahkan Model Category - Migrasi Model Category ke DB

--- bookStoreV2/bookStoreV2.DataAccess/Data/ApplicationDbContext.cs
+++ bookStoreV2/bookStoreV2.DataAccess/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using bookStoreV2.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace bookStoreV2.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
---
+++ bookStoreV2/bookStoreV2.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace bookStoreV2.Models
+{
+//pastikan merupakan public class agar bisa diakses dr luar
+    public class Category
+    {
+        //buat property Id dan Name
+        //jika ada nama property Id, maka akan diidentifikasi langsung sbg Prim Key
+        //jika namanya bukan Id namun ingin dijadikan Id, pastikan data annotation [Key]
+
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public String Name { get; set; }
+    }
+}
