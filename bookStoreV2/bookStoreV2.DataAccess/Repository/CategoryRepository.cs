c9177f9b80c6318dc3a085b01f690018efb75c9a
surya-ast <surya.astana71@gmail.com>
2022-01-10 22:14:19 +0700

    menambahkan Category Repository dan Interface -nya

---
+++ bookStoreV2/bookStoreV2.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using bookStoreV2.DataAccess.Data;
+using bookStoreV2.DataAccess.Repository.IRepository;
+using bookStoreV2.Models;
+
+namespace bookStoreV2.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if(objFromDb != null)
+            { 
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ bookStoreV2/bookStoreV2.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using bookStoreV2.Models;
+
+namespace bookStoreV2.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
